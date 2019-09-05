506c46ced0bd5ebd78edeca31f2626d7b05ad83b
primkey7607 <psubramaniam@uchicago.edu>
2019-09-05 14:51:43 -0500

    removed filesys test

--- test.py
+++ test.py
@@ -2,10 +2,17 @@ from dlstorage.utils.benchmark import *
 
 
 
+"""
 f = FileSystemStorageManager(TestTagger(), 'videos')
+<<<<<<< HEAD
 p = PerformanceTest(f, '/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4')
 p.runAll()
 
+=======
+f.put('/Users/sanjaykrishnan/Downloads/BigBuckBunny.mp4', 'bunny')
+print(f.get('bunny', TRUE, 60*30))
+"""
+>>>>>>> removed filesys test
 vd = VDMSStorageManager(TestTagger())
 vd.put('enter actual directory here', 'desired name')
 print(vd.get('desired name', TRUE, 438))

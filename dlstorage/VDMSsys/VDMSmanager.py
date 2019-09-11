818699d753ce875165bcf1c9bccf7398b5ec8605
P.M.Sub <psubramaniam@uchicago.edu>
2019-09-10 23:47:58 -0500

    fixed another subprocess bug

--- dlstorage/VDMSsys/VDMSmanager.py
+++ dlstorage/VDMSsys/VDMSmanager.py
@@ -56,9 +56,8 @@ class VDMSStorageManager(StorageManager):
             numClips = math.ceil(fsize / 32.0)
             #find duration of video file to compute clip duration
             result = subprocess.Popen(['ffprobe', filename], \
-                                      stdout = subprocess.PIPE, stderr = subprocess.STDOUT, \
-                                      encoding='ascii')
-            dur = [x for x in result.stdout.readlines() if "Duration" in x]
+                                      stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+            dur = [x for x in result.stdout.readlines().decode('ascii').splitlines(True) if "Duration" in x]
             #convert duration string to seconds
             inf = dur[0].split(',')
             dInfo = "".join(inf[0].split())

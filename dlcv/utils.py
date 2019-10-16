5fc2fb70c62a0cf9fd22099d3c20d635fb8f2c59
Ted Shaowang <tedswjz@gmail.com>
2019-10-16 13:30:53 -0500

    add labels_to_intervals()

--- dlcv/utils.py
+++ dlcv/utils.py
@@ -8,6 +8,7 @@ image streams.
 
 import cv2
 import numpy as np
+import itertools
 
 #plays video stream through the system player
 def play(vstream):
@@ -67,4 +68,35 @@ def image_match(im1, im2, hess_thresh=150, dist_threshold=1000, accept=0.75):
 		return False
 	return (match_cnt/len(kps1) >= accept)
 	
+def labels_to_intervals(labels_list):
+    """
+    labels_to_intervals() converts list of labels of each frame into set of time intervals where a tag occurs
 
+    Args:
+        labels_list: list of labels of each frame
+        e.g. [{'person'}, {'person'}, {'person'}, {'surfboard', 'person'}]
+
+    Returns:
+        tags - set of time intervals where a tag occurs:
+            { (label, start, end) }, a video from time 0 (inclusive) to time T (exclusive)
+            e.g. {('cat', 3, 9), ('dog', 5, 8), ('people', 0, 6)}
+            e.g. {('cat', 0, 1), ('cat', 2, 4), ('cat', 6, 8), ('dog', 0, 3),
+                  ('dog', 6, 8), ('people', 0, 2), ('people', 4, 6)}
+
+    """
+
+    labels_dict = dict()
+    for frame, labels in enumerate(labels_list):
+        for label in labels:
+            if label in labels_dict:
+                labels_dict[label].add(frame)
+            else:
+                labels_dict[label] = {frame}
+
+    output = set()
+    for key, value in labels_dict.items():
+        frame_list = sorted(value)
+        for interval in [(t[0][1], t[-1][1]) for t in
+                         (tuple(g[1]) for g in itertools.groupby(enumerate(frame_list), lambda x: x[0]-x[1]))]:
+            output.add((key, interval[0], interval[1]))
+    return output

3732846438066c393c2c08506bfbf550f73079fb
Ted Shaowang <tedswjz@gmail.com>
2019-11-05 15:01:43 -0600

    update test_detect.py

--- test_detect.py
+++ test_detect.py
@@ -8,6 +8,7 @@ from dlcv.dataflow.agg import *
 from dlcv.tracking.contour import *
 from dlcv.tracking.event import *
 from dlcv.object_detection.detect import *
+from timeit import default_timer as timer
 
 
 def boxes_to_labels(frame):
@@ -20,10 +21,20 @@ def boxes_to_labels(frame):
 
 
 if __name__ == '__main__':
-    v = VideoStream('cut.mp4', limit=10)
+    start = timer()
+    sys.setrecursionlimit(100000)
+
+    if len(sys.argv) < 2:
+        print("Enter filename as argv[1]")
+        exit(1)
+    filename = sys.argv[1]
+    v = VideoStream(filename, limit=10000)
     pipeline = v[Crop(100, 450, 450, 950)][
         TensorFlowObjectDetect(model_file='ssd_mobilenet_v1_coco_2017_11_17', label_file='mscoco_label_map.pbtxt',
                                num_classes=90, confidence=0.2)]
 
     labels = [boxes_to_labels(frame) for frame in pipeline]
     print(labels_to_intervals(labels))
+
+    end = timer()
+    print(end - start)

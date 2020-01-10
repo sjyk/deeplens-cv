4f1091a55dbf68c2c363232a10f71325ee816dc5
Ted Shaowang <tedswjz@gmail.com>
2020-01-10 17:39:48 -0600

    add BufferReduce class

---
+++ deeplens/dataflow/buffer_reduce.py
@@ -0,0 +1,140 @@
+import logging
+import time
+import cv2
+
+from deeplens.struct import Operator
+
+class BufferReduce(Operator):
+
+    def __init__(self,
+                 buffer_size=10, \
+                 sampling_rate=1, \
+                 resolution=1,
+                 alpha=1,
+                 ssthresh=300):
+        """buffer_reduce takes in five parameters:
+        the size of the buffer, a sampling rate, a resolution resizing and
+        two more for buffer size tuning - the alpha in EWMA and a slow-start threshold.
+        """
+
+        self.buffer_size = buffer_size
+        self.skip = int(1.0 / sampling_rate)
+        self.resolution = resolution
+        self.alpha = alpha  # the alpha in exponentially-weighted moving average
+        self.ssthresh = ssthresh  # slow start threshold
+        self.max_buffer_size = -1  # when max rate is reached, ssthresh is fixed
+
+    def __iter__(self):
+        self.frame_iter = iter(self.video_stream)
+        self.buffer = []
+        self.output = dict()
+        self.buffer_state = 0
+        self.rate = 0.0
+
+        self.super_iter()
+        return self
+
+    # this is the main routine which buffers up the frames and applies a reduce function
+    # to each buffer
+    def _reduce(self):
+        prediction_indices = range(0, min(self.buffer_size, len(self.buffer)), self.skip)
+        inp = [self.buffer[p] for p in prediction_indices]
+
+        if self.resolution == 1:
+            output = self.reduce([frame['data'] for frame in inp])
+        else:
+            output = self.reduce([cv2.resize(frame['data'],
+                                          (int(frame['data'].shape[1] * self.resolution), \
+                                           int(frame['data'].shape[0] * self.resolution))) \
+                               for frame in inp])
+
+        self.output = output
+
+    def reduce(self, data):
+        """reduce() must be implemented by each subclass.
+
+            Args:
+                data - list of raw frames
+
+            Returns:
+                output - dict of reduce outputs
+        """
+        raise NotImplemented("BufferReduce must implement a reduce function")
+
+    # fill up the buffer
+    def _fill_buffer(self):
+
+        for i in range(self.buffer_size):
+            frame = next(self.frame_iter, None)
+
+            if frame == None:
+                break
+
+            self.buffer.append(frame)
+
+        if len(self.buffer) > 0:
+            logging.debug("Frame No. %s", self.buffer[0]['frame'])
+
+    def _tune_buffer_size(self, rate, add_factor=50, multiply_factor=0.7):
+        # slow start
+        if self.buffer_size < self.ssthresh:
+            self.buffer_size *= 2
+
+        # additive-increase/multiplicative-decrease (AIMD)
+        if self.max_buffer_size == -1 and rate < self.rate:
+            self.ssthresh = int(self.buffer_size / 2)
+            self.buffer_size = int(self.buffer_size * multiply_factor) + 1
+            self.max_buffer_size = self.buffer_size
+        elif self.max_buffer_size != -1 and self.buffer_size > self.max_buffer_size and rate < self.rate:
+            self.buffer_size = int(self.buffer_size * multiply_factor) + 1
+        else:
+            self.buffer_size = int(self.buffer_size + add_factor)
+
+    # iterator producer
+    def __next__(self):
+        self.super_next()
+
+        if self.buffer_state == 0:
+            # buffer is filled so run reduce first time
+            logging.debug("buffer_state: 0. buffer_size: %s", self.buffer_size)
+            now = time.time()
+            self._fill_buffer()
+
+            if len(self.buffer) == 0:
+                raise StopIteration()
+
+            self._reduce()
+            delta = time.time() - now
+            logging.debug("Time taken for _fill_buffer and _reduce: %s", delta)
+            logging.debug("Average rate before: %s", self.rate)
+
+            rate = self.buffer_size / delta
+            self._tune_buffer_size(rate)
+
+            logging.debug("Rate this time: %s", rate)
+
+            # exponentially-weighted moving average
+            if self.rate == 0:
+                self.rate = rate
+            else:
+                self.rate = self.alpha * rate + (1 - self.alpha) * self.rate
+            logging.debug("Average rate after: %s", self.rate)
+
+        # cache the current buffer index
+        index = self.buffer_state
+
+        if self.buffer_state == (len(self.buffer) - 1):
+            # at the end
+            self.buffer = []
+            self.buffer_state = 0
+        else:
+            # at the middle
+            self.buffer_state += 1
+
+        return self.output
+
+class TestBufferReduce(BufferReduce):
+
+    def reduce(self, data):
+        print('[Buffer Reduce] Processed: ',len(data), 'frames')
+        return {'number of frames': len(data)}

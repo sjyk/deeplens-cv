939b2c88c388781746bc6caed002a839878d9375
Barbarioli <bbarbarioli@hotmail.com>
2020-01-17 16:07:48 -0600

    test with various sampling procedures

--- test.py
+++ test.py
@@ -11,24 +11,34 @@ from dlcv.tracking.event import *
 
 import cv2
 import numpy as np
+import sys
 
+sys.setrecursionlimit(10000)
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+frame_limit = 5000
 
 #count the number of cars in the left lane
-v = VideoStream('tcam.mp4', limit=1000)
+print("\nRegular counting: number of cars in the left lane")
+v = VideoStream('/Users/brunobarbarioli/Documents/Research/videos/tcam.mp4', limit=frame_limit)
 region = Box(200,550,350,750)
 pipeline = v[KeyPoints()][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
 print('Left', count(pipeline, ['one'], stats=True))
+"""
 #print([(p.__class__.__name__, p.serialize()) for p in pipeline.lineage()[1:]])
 
 #count the number of cars in the right lane
+print("\nRegular counting: number of cars in the right lane")
 v = VideoStream('tcam.mp4', limit=1000)
 region = Box(500,550,650,750)
 pipeline = v[KeyPoints()][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
 print('Right', count(pipeline, ['one'], stats=True))
 
 
-
 #optimize car counting by pushing down a crop()
+print("\nOptimized car counting by pushing down a crop: number of cars in the left lane")
 v = VideoStream('tcam.mp4', limit=1000)
 region = Box(200,550,350,750)
 
@@ -38,11 +48,27 @@ k.setCrop(Box(100,450,450,950)) #notice it is slightly bigger than the actual re
 pipeline = v[k][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
 print('Left', count(pipeline, ['one'], stats=True))
 
-
-#Optimize car counting by sampling
-v = VideoStream('tcam.mp4', limit=1000)
+"""
+#Optimize car counting by  naive sampling
+print("\nOptimized car counting by naive sampling: number of cars in the left lane")
+v = VideoStream('/Users/brunobarbarioli/Documents/Research/videos/tcam.mp4', limit=frame_limit)
 region = Box(200,550,350,750)
 pipeline = v[Sample(0.5)][KeyPoints()][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
 print('Left', count(pipeline, ['one'], stats=True))
 
+#Optimize car counting by  EBGS sampling
+print("\nOptimized car counting by EBS sampling: number of cars in the left lane")
+v = VideoStream('/Users/brunobarbarioli/Documents/Research/videos/tcam.mp4', limit=frame_limit)
+region = Box(200,550,350,750)
+pipeline = v[EBGS(0.5, 0.01, 5)][KeyPoints()][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
+print('Left', count(pipeline, ['one'], stats=True))
+
+#Optimize car counting by random uniform sampling
+print("\nOptimized car counting by random uniform sampling: number of cars in the left lane")
+v = VideoStream('/Users/brunobarbarioli/Documents/Research/videos/tcam.mp4', limit=frame_limit)
+region = Box(200,550,350,750)
+pipeline = v[RUS(3)][KeyPoints()][ActivityMetric('one', region)][Filter('one', [-0.25,-0.25,1,-0.25,-0.25],1.5, delay=10)]
+print('Left', count(pipeline, ['one'], stats=True))
+
+#Optimize car counting by block sampling
 

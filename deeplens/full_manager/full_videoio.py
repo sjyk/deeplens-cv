2f49748b330c5b4ffb49f7fc967c2d88e8ed2fdc
Ted Shaowang <tedswjz@gmail.com>
2020-02-03 00:02:16 -0600

    more video_io functions

--- deeplens/full_manager/full_videoio.py
+++ deeplens/full_manager/full_videoio.py
@@ -56,31 +56,86 @@ def write_video_auto(vstream, \
 
 
 #delete a video
-def delete_video_if_exists(cursor, video_name):
-
-    cursor.execute("SELECT background_id, clip_id FROM background WHERE video_name = '%s'" % video_name)
-    clips = cursor.fetchall()
+def delete_video_if_exists(conn, video_name):
+    c = conn.cursor()
+    c.execute("SELECT background_id, clip_id FROM background WHERE video_name = '%s'" % video_name)
+    clips = c.fetchall()
     if len(clips) == 0:
         # not exist in header file, nothing to do
         return
 
     clips = set().union(*map(set, clips))
     for clip in clips:
-        cursor.execute("SELECT video_ref FROM clip WHERE clip_id = '%d'" % clip)
-        video_ref = cursor.fetchone()[0]
+        c.execute("SELECT video_ref FROM clip WHERE clip_id = '%d'" % clip)
+        video_ref = c.fetchone()[0]
         try:
             os.remove(video_ref)
         except FileNotFoundError:
             logging.warning("File %s not found" % video_ref)
-        cursor.execute("DELETE FROM clip WHERE clip_id = '%d'" % clip)
+        c.execute("DELETE FROM clip WHERE clip_id = '%d'" % clip)
 
-    cursor.execute("DELETE FROM background WHERE video_name = '%s'" % video_name)
-    cursor.commit()
+    c.execute("DELETE FROM background WHERE video_name = '%s'" % video_name)
+    conn.commit()
 
 
-def move_one_file(cursor, clip_id, dest_ref):
-    cursor.execute("SELECT video_ref FROM clip WHERE clip_id = '%d'" % clip_id)
-    video_ref = cursor.fetchone()[0]
+def move_one_file(conn, clip_id, dest_ref):
+    c = conn.cursor()
+    c.execute("SELECT video_ref FROM clip WHERE clip_id = '%d'" % clip_id)
+    video_ref = c.fetchone()[0]
     shutil.move(video_ref, dest_ref)
-    cursor.execute("UPDATE clip SET video_ref = '%s' WHERE clip_id = '%d'" % (dest_ref, clip_id))
-    cursor.commit()
+    c.execute("UPDATE clip SET video_ref = '%s' WHERE clip_id = '%d'" % (dest_ref, clip_id))
+    conn.commit()
+
+
+def insert_clip(conn, clip_id, start_time, end_time, origin_x, origin_y, height, width,
+                has_label, labels='', video_ref=''):
+    c = conn.cursor()
+    c.execute("INSERT INTO clip VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   (clip_id, start_time, end_time, origin_x, origin_y, height, width, has_label, labels, video_ref))
+    conn.commit()
+
+
+def insert_background(conn, background_id, clip_id, video_name):
+    c = conn.cursor()
+    c.execute("INSERT INTO background VALUES (?, ?, ?)", (background_id, clip_id, video_name))
+    conn.commit()
+
+
+def delete_clip(conn, clip_id):
+    c = conn.cursor()
+    c.execute("SELECT video_ref FROM clip WHERE clip_id = '%d'" % clip_id)
+    video_ref = c.fetchone()[0]
+    try:
+        os.remove(video_ref)
+    except FileNotFoundError:
+        logging.warning("File %s not found" % video_ref)
+    c.execute("DELETE FROM clip WHERE clip_id = '%d'" % clip_id)
+    conn.commit()
+
+
+def update_clip(conn, clip_id, args={}):
+    c = conn.cursor()
+    for key, value in args.items():
+        c.execute("UPDATE clip SET '%s' = '%s' WHERE clip_id = '%d'" % (key, value, clip_id))
+    conn.commit()
+
+
+def query_clip(conn, clip_id):
+    c = conn.cursor()
+    c.execute("SELECT * FROM clip WHERE clip_id = '%d'" % clip_id)
+    result = c.fetchall()
+    return result
+
+
+def query_background(conn, background_id=None, clip_id=None):
+    c = conn.cursor()
+    if background_id == None and clip_id == None:
+        raise ValueError("Neither background_id nor clip_id is given")
+    elif background_id != None and clip_id != None:
+        c.execute("SELECT * FROM background WHERE background_id = '%d' AND clip_id = '%d'" % (background_id, clip_id))
+    elif background_id != None and clip_id == None:
+        c.execute("SELECT * FROM background WHERE background_id = '%d'" % background_id)
+    elif background_id == None and clip_id != None:
+        c.execute("SELECT * FROM background WHERE clip_id = '%d'" % clip_id)
+    result = c.fetchall()
+    return result

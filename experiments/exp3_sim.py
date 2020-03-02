b072a7f2abbb660bc1dccae7b92d033fb46eae9d
Sanjay Krishnan <sanjaykrishn@gmail.com>
2020-03-02 17:20:01 -0600

    Updated the sim function and made sure we get the right answer!

--- experiments/exp3_sim.py
+++ experiments/exp3_sim.py
@@ -30,7 +30,7 @@ def runNaive(src, tot=1000, sel=0.1):
 	sel = sel/2
 	region = Box(500, 350, 750, 450)
 	pipelines = c[Cut(tot//2-int(tot*sel),tot//2+int(tot*sel))][KeyPoints(blur=3)][ActivityMetric('one', region)][Filter('one', [-0.5,1,-0.5], 0.25, delay=40)]
-	result = count(pipelines, ['one'], stats=True)[1]['elapsed']
+	result = count(pipelines, ['one'], stats=True)
 
 	logrecord('naive',({'size': tot, 'sel': sel, 'file': src}), 'get', str(result), 's')
 
@@ -51,7 +51,7 @@ def runSimple(src, tot=1000, sel=0.1):
 	for c in clips:
 		pipelines.append(c[KeyPoints(blur=3)][ActivityMetric('one', region)][Filter('one', [-0.5,1,-0.5], 0.25, delay=40)])
 
-	result = counts(pipelines, ['one'], stats=True)[1]['elapsed']
+	result = counts(pipelines, ['one'], stats=True)
 
 	logrecord('simple',({'size': tot, 'sel': sel, 'file': src}), 'get', str(result), 's')
 
@@ -60,10 +60,10 @@ def runSimple(src, tot=1000, sel=0.1):
 def runFull(src, tot=1000, sel=0.1):
 	cleanUp()
 
-	manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter(movement_threshold=21,blur=7,movement_prob=0.99).segment, batch_size=100), CropSplitter(), 'videos')
+	manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter(movement_threshold=21,blur=7,movement_prob=0.10).segment, batch_size=100), CropSplitter(), 'videos')
 	manager.put(src, 'test', args={'encoding': XVID, 'size': -1, 'sample': 1.0, 'offset': 0, 'limit': tot, 'batch_size': 100})
 
-	region = Box(200, 550, 350, 750)
+	region = Box(500, 350, 750, 450)
 	sel = sel/2
 
 	clips = manager.get('test', Condition(label='foreground', custom_filter=time_filter(tot//2-int(tot*sel),tot//2+int(tot*sel))))
@@ -72,7 +72,7 @@ def runFull(src, tot=1000, sel=0.1):
 	for c in clips:
 		pipelines.append(c[KeyPoints(blur=3)][ActivityMetric('one', region)][Filter('one', [-0.5,1,-0.5], 0.25, delay=40)])
 
-	result = counts(pipelines, ['one'], stats=True)[1]['elapsed']
+	result = counts(pipelines, ['one'], stats=True)
 
 	logrecord('full',({'size': tot, 'sel': sel, 'file': src}), 'get', str(result), 's')
 
@@ -82,10 +82,10 @@ def runFull(src, tot=1000, sel=0.1):
 def runFullOpt(src, tot=1000, sel=0.1):
 	cleanUp()
 
-	manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter(movement_threshold=21,blur=7,movement_prob=0.99).segment, batch_size=100), CropSplitter(), 'videos')
+	manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter(movement_threshold=21,blur=7,movement_prob=0.10).segment, batch_size=100), CropSplitter(), 'videos')
 	manager.put(src, 'test', args={'encoding': XVID, 'size': -1, 'sample': 1.0, 'offset': 0, 'limit': tot, 'batch_size': 100})
 
-	region = Box(200, 550, 350, 750)
+	region = Box(500, 350, 750, 450)
 	sel = sel/2
 	
 	clips = manager.get('test', Condition(label='foreground',custom_filter=time_filter(tot//2-int(tot*sel),tot//2+int(tot*sel))))
@@ -97,9 +97,9 @@ def runFullOpt(src, tot=1000, sel=0.1):
 		pipeline = d.optimize(pipeline)
 		pipelines.append(pipeline)
 
-	result = counts(pipelines, ['one'], stats=True)[1]['elapsed']
+	result = counts(pipelines, ['one'], stats=True)
 
 	logrecord('fullopt',({'size': tot, 'sel': sel, 'file': src}), 'get', str(result), 's')
 
 
-do_experiments(sys.argv[1], [runNaive, runSimple, runFull, runFullOpt], 1000, range(2,10))
+do_experiments(sys.argv[1], [runFull], 10000, range(2,10))

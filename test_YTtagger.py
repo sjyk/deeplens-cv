23b99b5406edc20bb13ca5446baf48fa590353b1
P.M.Sub <psubramaniam@uchicago.edu>
2020-03-06 18:40:29 -0600

    added actual test file...

---
+++ test_YTtagger.py
@@ -0,0 +1,10 @@
+from deeplens.media.youtube_tagger import YoutubeTagger
+"""
+I literally just want to make sure this iterator does what I think it does
+I'm not going to worry about whether it's compatible with the storage manager
+for now...
+"""
+youtubeTagger = YoutubeTagger('./train/AAI0cDTWFvE.mp4', './train/processed_yt_bb_detection_train.csv')
+for frame in youtubeTagger:
+    print(frame)
+

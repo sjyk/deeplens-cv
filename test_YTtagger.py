71f52f039d922fe6ab821a11377edc4a800d0312
P.M.Sub <psubramaniam@uchicago.edu>
2020-03-06 19:33:51 -0600

    infinite loop bug

--- test_YTtagger.py
+++ test_YTtagger.py
@@ -6,5 +6,10 @@ for now...
 """
 youtubeTagger = YoutubeTagger('./train/AAI0cDTWFvE.mp4', './train/processed_yt_bb_detection_train.csv')
 for frame in youtubeTagger:
-    print(frame)
+    bb= frame['bb']
+    x0 = str(bb.x0)
+    x1 = str(bb.x1)
+    y0 = str(bb.y0)
+    y1 = str(bb.y1)
+    print(frame['label'] + ',' + x0 + ',' + x1 + ',' + y0 + ',' + y1)
 

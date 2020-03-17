a920fc139fd14a44231e9fc69025dea289242ea1
Sanjay Krishnan <sanjaykrishn@gmail.com>
2020-03-17 11:29:08 -0500

    Updated with a materialize function

--- deeplens/extern/cache.py
+++ deeplens/extern/cache.py
@@ -6,7 +6,13 @@ def persist(vstream, file):
     materialized_stream = [frame['data'] for frame in vstream]
     np.savez(file, *materialized_stream)
 
-def load(file):
+
+#does not currently handle meta, returns a ref to the meta data
+def materialize(vstream):
+    materialized_stream = [frame['data'] for frame in vstream]
+    return RawVideoStream(materialized_stream)
+
+def load(materialized_stream):
     npzfile = np.load(file)
     materialized_stream = [npzfile[arr] for arr in npzfile.files]
     return RawVideoStream(materialized_stream)

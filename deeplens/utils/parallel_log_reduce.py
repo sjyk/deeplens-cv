a7b519c9fe7b016c84b33b10d4cca31f7a643a31
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-03-30 18:58:06 -0500

    allow full manager to create a new connection on new manager function calls

--- deeplens/utils/parallel_log_reduce.py
+++ deeplens/utils/parallel_log_reduce.py
@@ -2,13 +2,13 @@
 is copyrighted by the University of Chicago. This project is developed by
 the database group (chidata).
 
-paralleL_log_reduce.py calculates approximate intermidiate times for put
+parallel_log_reduce.py calculates approximate intermidiate times for put
 
 """ 
 import json
 import os
 
-def paralleL_log_reduce(logs, start_time):
+def parallel_log_reduce(logs, start_time):
     times = []
     for log in logs:
         with open(log, 'r') as f:

e5ffd827632178ffb3071ffba1cab8fac78c8b12
j2zhao <jinjin@MacBook-Pro.local>
2020-06-26 09:10:46 -0500

    keypoint tests

--- test_get.py
+++ test_get.py
@@ -5,10 +5,13 @@ from deeplens.tracking.contour import *
 from deeplens.dataflow.map import *
 from deeplens.full_manager.full_manager import *
 from deeplens.full_manager.video_processing_keypoint import *
+from deeplens.object_detection.detect import *
 
 
-manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter().segment, batch_size=30), CropSplitter(), 'videos')
-manager = FullStorageManager(SizeMovementTagger(), CropSplitter(), 'videos')
+#manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter().segment, batch_size=30), CropSplitter(), 'videos')
+#manager = FullStorageManager(SizeMovementTagger(), CropSplitter(), 'videos')
+manager = FullStorageManager(TensorFlowObjectDetect(model_file='ssd_mobilenet_v1_coco_2017_11_17', label_file='mscoco_label_map.pbtxt',
+                               num_classes=90, confidence=0.25), CropSplitter(), 'videos')
 manager.put('./cut3.mp4', 'test2')
 #res = manager.get('test', Condition(label='small'))
 #print([video for video in res[0]][0]['data'].shape)

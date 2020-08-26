0240f60b3899f64f60e1c6659d7df3a1e204f457
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2020-08-26 15:17:56 -0500

    boxes done gotta fix bugs

--- diagnostics_dante/graph_diagnostics.py
+++ diagnostics_dante/graph_diagnostics.py
@@ -5,9 +5,50 @@ from matplotlib import pyplot as plt
 def graph_diagnostics(path_to_csv, path_to_agg_csv):
     os.mkdir('results/graphs')
     os.mkdir('results/graphs/spread')
+    os.mkdir('results/graphs/file_size')
     os.mkdir('results/graphs/total_time')
 
+    dfo = pd.read_csv(path_to_csv)
+    for shape, dfi in dfo.groupby('Shape'):
+        storage_time = []
+        retrieval_time = []
+
+        for size, df in dfi.groupby('Size'):
+            storage_time.append(df['Storage Time'].tolist())
+            retrieval_time.append(df['Retrieval Time'].tolist())
+
+        fig = plt.figure(figsize = (10, 5)) 
+        ax2 = fig.add_axes([0, 0, 1, 1])
+        bp2 = ax2.boxplot(storage_time)
+        plt.savefig(f"results/graphs/spread/{shape}_storagetime.png")
+        fig.clf()
+
+        fig = plt.figure(figsize = (10, 5)) 
+        ax3 = fig.add_axes([0, 0, 1, 1])
+        bp3 = ax3.boxplot(retrieval_time)
+        plt.savefig(f"results/graphs/spread/{shape}_retrievaltime.png")
+        fig.clf()
+
     dfa = pd.read_csv(path_to_agg_csv)
+    for shape, df in dfa.groupby('Shape'):
+        df = df.reset_index()
+        
+        fig = plt.figure(figsize = (10, 5)) 
+        plt.bar(df['Size'], height=df['File Size Median']) 
+        plt.xlabel("Video Size (pixels)") 
+        plt.ylabel("Cache size (bytes)") 
+        plt.title(f"{shape} File Size") 
+        plt.savefig(f"results/graphs/file_size/{shape}_filesize.png")
+        fig.clf()
+
+        fig = plt.figure(figsize = (10, 5))
+        p1 = plt.bar(df['Size'], height=df['Storage Time Median'])
+        p2 = plt.bar(df['Size'], height=df['Retrieval Time Median'], bottom=df['Storage Time Median'])
+        plt.xlabel("Video Size (pixels)") 
+        plt.ylabel("Total Time (seconds)") 
+        plt.title(f"{shape} Total Time") 
+        plt.savefig(f"results/graphs/total_time/{shape}_totaltime.png")
+        fig.clf()
 
     # for size, df in dfo.groupby('Size'):
     #     fig0 = plt.figure(figsize = (10, 5))
@@ -18,40 +59,8 @@ def graph_diagnostics(path_to_csv, path_to_agg_csv):
     #     plt.title(f"{size} Total Time") 
     #     plt.savefig(f"results/graphs/{size}_totaltime.png")
 
-    for shape, df in dfa.groupby('Shape'):
-        df = df.reset_index()
-        
-        # fig1 = plt.figure(figsize = (10, 5)) 
-        # plt.bar(df['Size'], height=df['File Size Max'], bottom=df['File Size Min']) 
-        # plt.xlabel("Video Size (pixels)") 
-        # plt.ylabel("Cache size (bytes)") 
-        # plt.title(f"{shape} File Size") 
-        # plt.savefig(f"results/graphs/{shape}_filesize.png")
-
-        # fig2 = plt.figure(figsize = (10, 5)) 
-        # plt.bar(df['Size'], height=df['Storage Time Max'], bottom=df['Storage Time Min'])   
-        # plt.xlabel("Video Size (pixels)") 
-        # plt.ylabel("Storage Time (seconds)") 
-        # plt.title(f"{shape} Storage Time") 
-        # plt.savefig(f"results/graphs/{shape}_storagetime.png")
-
-        # fig3 = plt.figure(figsize = (10, 5)) 
-        # plt.bar(df['Size'], height=df['Retrieval Time Max'], bottom=df['Retrieval Time Min'])   
-        # plt.xlabel("Video Size (pixels)") 
-        # plt.ylabel("Retrieval Time (seconds)") 
-        # plt.title(f"{shape} Retrieval Time") 
-        # plt.savefig(f"results/graphs/{shape}_retrievaltime.png")
-
-        fig3 = plt.figure(figsize = (10, 5))
-        p1 = plt.bar(df['Size'], height=df['Storage Time Median'])
-        p2 = plt.bar(df['Size'], height=df['Retrieval Time Median'], bottom=df['Storage Time Median'])
-        plt.xlabel("Video Size (pixels)") 
-        plt.ylabel("Total Time (seconds)") 
-        plt.title(f"{shape} Total Time") 
-        plt.savefig(f"results/graphs/total_time/{shape}_totaltime.png")
-
     path_to_graphs = 'results/graphs' # A folder of graphs
     return path_to_graphs
 
 # Test
-graph_diagnostics("diagnostics.csv", "results/diagnostics_agg.csv")
+graph_diagnostics("results/diagnostics.csv", "results/diagnostics_agg.csv")
